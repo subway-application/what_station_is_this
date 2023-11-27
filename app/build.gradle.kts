8240adc6187453c642d637c42370f1150ea1df3c
ydh <128958284+ydh1005@users.noreply.github.com>
2023-11-27 19:03:19 +0900

    1차 시험

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.subway"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.subway"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
