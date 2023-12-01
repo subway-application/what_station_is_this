f2fbb01e62ef62c185b7d14d6f92a27ca8ea5522
ydh <128958284+ydh1005@users.noreply.github.com>
2023-12-01 16:16:00 +0900

    우선 노선도

--- app/src/main/java/com/example/subway/PathsDirections/Edge.kt
+++ app/src/main/java/com/example/subway/PathsDirections/Edge.kt
@@ -1,8 +1,11 @@
 package com.example.subway.PathsDirections
 
-data class Edge(
-    val destination: Node,
-    val time: Int,
-    val distance: Int,
-    val cost: Int
-)
+class Edge(val destination: Node, val time: Int, private val distance: Int, val cost: Int) {
+    override fun hashCode(): Int {
+        var result = destination.id
+        result = 31 * result + time
+        result = 31 * result + distance
+        result = 31 * result + cost
+        return result
+    }
+}
--- app/src/main/java/com/example/subway/PathsDirections/Node.kt
+++ app/src/main/java/com/example/subway/PathsDirections/Node.kt
@@ -2,7 +2,15 @@ package com.example.subway.PathsDirections
 
 import com.example.subway.PathsDirections.Edge
 
-data class Node(
-    val station: Int,
+class Node(val id: Int) {
+    //var predecessor: Nothing? = null
+    //var visited: Boolean = false
     val edges: MutableList<Edge> = mutableListOf()
-)
+    val lines = mutableSetOf<Int>() // 각 노드가 속한 노선 정보를 저장하는 리스트
+    override fun hashCode(): Int {
+        return id
+    }
+    override fun toString(): String {
+        return id.toString()
+    }
+}
